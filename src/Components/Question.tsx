b62c3c0976a72620634aeebd8f0ab07c671df9dd
DiegoFavianGarcia <47956053+diegofavian94@users.noreply.github.com>
2024-01-18 02:22:49 -0500

    Category decoding

--- src/Components/Question.tsx
+++ src/Components/Question.tsx
@@ -7,9 +7,9 @@ export const Question: React.FC<{category:String, question:String, number:number
 }) => {
   return (
     <div className=" h-70 flex flex-column justify-between">
-        <p className="f2 b">{category}</p>
-        <div className="ba pv4 f3">{useDecodeText(question)}</div>
-        <div className="f3">{number+" out of 12"}</div>
+        <p className="f2 b">{useDecodeText(category)}</p>
+        <div className="ba pv4 f3 ph2">{useDecodeText(question)}</div>
+        <div className="f3 ma3">{number+" out of 12"}</div>
     </div>
     
   )
